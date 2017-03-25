d619ce88217844550cad0019abd25ff0f4331811
Brandon Keepers <bkeepers@github.com>
2017-03-25 08:37:24 -0500

    Check DCO for PRs

--- index.js
+++ index.js
@@ -10,9 +10,33 @@ module.exports = (robot) => {
         sha: commit.id,
         state: signedOff ? 'success' : 'failure',
         target_url: 'https://developercertificate.org/',
-        context: "DCO",
+        context: "DCO/commit",
         description: 'git commit --signoff'
       }));
     });
   });
+
+  robot.on('pull_request.opened', sync);
+  robot.on('pull_request.synchronize', sync);
+
+  async function sync (event, context) {
+    const github = await robot.auth(event.payload.installation.id);
+    const pr = event.payload.pull_request;
+
+    const compare = await github.repos.compareCommits(context.repo({
+      base: pr.base.sha,
+      head: pr.head.sha
+    }));
+
+    const signedOff = compare.commits.every(data => dco(data.commit.message));
+
+    return github.repos.createStatus(context.repo({
+      sha: pr.head.sha,
+      state: signedOff ? 'success' : 'failure',
+      target_url: 'https://developercertificate.org/',
+      context: "DCO/pr",
+      description: 'git commit --signoff'
+    }));
+
+  }
 };
