4dc778fc9682341b7c4e70cfc15cccc875289e77
Lóránt Pintér <lorant.pinter@gmail.com>
2018-01-13 22:00:54 +0100

    Use more extensible configuration format
    Signed-off-by: Lóránt Pintér <lorant.pinter@gmail.com>

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ module.exports = robot => {
   robot.on(['pull_request.opened', 'pull_request.synchronize'], check)
 
   async function check (context) {
-    const config = await context.config('dco.yml', {'require_signoff_for_members': true})
-    const requireSignoffForMembers = config.require_signoff_for_members
+    const config = await context.config('dco.yml', {
+      require: {
+        members: true
+      }
+    })
+    const requireForMembers = config.require.members
 
     const pr = context.payload.pull_request
     const organization = context.payload.organization.login
@@ -40,7 +44,7 @@ module.exports = robot => {
       return result
     }
 
-    const dcoParams = await getDCOStatus(compare.data.commits, requireSignoffForMembers
+    const dcoParams = await getDCOStatus(compare.data.commits, requireForMembers
       ? async () => true
       : async (author) => {
         const member = await isOrgMember(author)
