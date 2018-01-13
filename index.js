c3ce64c12e45c78a98e9880a3bde552d6fdea7da
Lóránt Pintér <lorant.pinter@gmail.com>
2018-01-13 15:29:22 +0100

    Do not require sign-off for organization members
    When `.github/dco.yml` specifies `require_signoff_for_members: false`,
    members of the repository's owning organization can skip signing off
    their commits, provided the commits are verified.
    
    Signed-off-by: Lóránt Pintér <lorant.pinter@gmail.com>

--- index.js
+++ index.js
@@ -4,14 +4,49 @@ module.exports = robot => {
   robot.on(['pull_request.opened', 'pull_request.synchronize'], check)
 
   async function check (context) {
+    const config = await context.config('dco.yml', {'require_signoff_for_members': true})
+    const requireSignoffForMembers = config.require_signoff_for_members
+
     const pr = context.payload.pull_request
+    const organization = context.payload.organization.login
 
     const compare = await context.github.repos.compareCommits(context.repo({
       base: pr.base.sha,
       head: pr.head.sha
     }))
 
-    const dcoParams = getDCOStatus(compare.data.commits)
+    const members = {}
+
+    const isOrgMember = async function (login) {
+      if (members.hasOwnProperty(login)) {
+        return members[login]
+      }
+
+      let result
+      try {
+        await context.github.orgs.checkMembership({
+          org: organization,
+          username: login
+        })
+        result = true
+      } catch (err) {
+        if (err.code === 404) {
+          result = false
+        } else {
+          throw err
+        }
+      }
+      members[login] = result
+      return result
+    }
+
+    const dcoParams = await getDCOStatus(compare.data.commits, requireSignoffForMembers
+      ? async () => true
+      : async (author) => {
+        const member = await isOrgMember(author)
+        return !member
+      }
+    )
 
     const params = Object.assign({
       sha: pr.head.sha,
