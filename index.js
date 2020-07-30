071a77a78752137730d830cb1e8381095bbf032c
Gregor Martynus <gregor@martynus.net>
2020-07-30 11:43:44 -0700

    fix: address deprecation about `.repos.createStatus` (#131)

--- index.js
+++ index.js
@@ -12,147 +12,143 @@ module.exports = (app) => {
   )
 
   async function check (context) {
-    try {
-      const timeStart = new Date()
+    const timeStart = new Date()
 
-      const config = await context.config('dco.yml', {
-        require: {
-          members: true
-        }
-      })
-      const requireForMembers = config.require.members
-
-      const pr = context.payload.pull_request
-
-      const compare = await context.github.repos.compareCommits(
-        context.repo({
-          base: pr.base.sha,
-          head: pr.head.sha
-        })
-      )
-
-      const commits = compare.data.commits
-      const dcoFailed = await getDCOStatus(
-        commits,
-        requireMembers(requireForMembers, context),
-        context.payload.pull_request.html_url
-      )
-
-      if (!dcoFailed.length) {
-        await context.github.checks
-          .create(
-            context.repo({
-              name: 'DCO',
-              head_branch: pr.head.ref,
-              head_sha: pr.head.sha,
-              status: 'completed',
-              started_at: timeStart,
-              conclusion: 'success',
-              completed_at: new Date(),
-              output: {
-                title: 'DCO',
-                summary: 'All commits are signed off!'
-              }
-            })
-          )
-          .catch(function checkFails (error) {
-            if (error.code === 403) {
-              console.log('resource not accessible, creating status instead')
-              // create status
-              const params = {
-                sha: pr.head.sha,
-                context: 'DCO',
-                state: 'success',
-                description: 'All commits are signed off!',
-                target_url: 'https://github.com/probot/dco#how-it-works'
-              }
-              return context.github.repos.createStatus(context.repo(params))
-            }
-          })
-      } else {
-        let summary = []
-        dcoFailed.forEach(function (commit) {
-          summary.push(
-            `Commit sha: [${commit.sha.substr(0, 7)}](${commit.url}), Author: ${
-              commit.author
-            }, Committer: ${commit.committer}; ${commit.message}`
-          )
-        })
-        summary = summary.join('\n')
-        if (dcoFailed.length === 1) { summary = handleOneCommit(pr, dcoFailed) + `\n\n${summary}` } else {
-          summary =
-            handleMultipleCommits(pr, commits.length, dcoFailed) +
-            `\n\n${summary}`
-        }
-
-        await context.github.checks
-          .create(
-            context.repo({
-              name: 'DCO',
-              head_branch: pr.head.ref,
-              head_sha: pr.head.sha,
-              status: 'completed',
-              started_at: timeStart,
-              conclusion: 'action_required',
-              completed_at: new Date(),
-              output: {
-                title: 'DCO',
-                summary
-              },
-              actions: [
-                {
-                  label: 'Set DCO to pass',
-                  description: 'would set status to passing',
-                  identifier: 'override'
-                }
-              ]
-            })
-          )
-          .catch(function checkFails (error) {
-            if (error.code === 403) {
-              console.log('resource not accessible, creating status instead')
-              // create status
-              const description = dcoFailed[dcoFailed.length - 1].message.substring(0, 140)
-              const params = {
-                sha: pr.head.sha,
-                context: 'DCO',
-                state: 'failure',
-                description,
-                target_url: 'https://github.com/probot/dco#how-it-works'
-              }
-              return context.github.repos.createStatus(context.repo(params))
-            }
-          })
+    const config = await context.config('dco.yml', {
+      require: {
+        members: true
       }
-    } catch (error) {
-      console.error(error)
+    })
+    const requireForMembers = config.require.members
+
+    const pr = context.payload.pull_request
+
+    const compare = await context.github.repos.compareCommits(
+      context.repo({
+        base: pr.base.sha,
+        head: pr.head.sha
+      })
+    )
+
+    const commits = compare.data.commits
+    const dcoFailed = await getDCOStatus(
+      commits,
+      requireMembers(requireForMembers, context),
+      context.payload.pull_request.html_url
+    )
+
+    if (!dcoFailed.length) {
+      await context.github.checks
+        .create(
+          context.repo({
+            name: 'DCO',
+            head_branch: pr.head.ref,
+            head_sha: pr.head.sha,
+            status: 'completed',
+            started_at: timeStart,
+            conclusion: 'success',
+            completed_at: new Date(),
+            output: {
+              title: 'DCO',
+              summary: 'All commits are signed off!'
+            }
+          })
+        )
+        .catch(function checkFails (error) {
+          if (error.status === 403) {
+            context.log.info('resource not accessible, creating status instead')
+            // create status
+            const params = {
+              sha: pr.head.sha,
+              context: 'DCO',
+              state: 'success',
+              description: 'All commits are signed off!',
+              target_url: 'https://github.com/probot/dco#how-it-works'
+            }
+            return context.github.repos.createCommitStatus(context.repo(params))
+          }
+
+          throw error
+        })
+    } else {
+      let summary = []
+      dcoFailed.forEach(function (commit) {
+        summary.push(
+          `Commit sha: [${commit.sha.substr(0, 7)}](${commit.url}), Author: ${
+            commit.author
+          }, Committer: ${commit.committer}; ${commit.message}`
+        )
+      })
+      summary = summary.join('\n')
+      if (dcoFailed.length === 1) { summary = handleOneCommit(pr, dcoFailed) + `\n\n${summary}` } else {
+        summary =
+          handleMultipleCommits(pr, commits.length, dcoFailed) +
+          `\n\n${summary}`
+      }
+
+      await context.github.checks
+        .create(
+          context.repo({
+            name: 'DCO',
+            head_branch: pr.head.ref,
+            head_sha: pr.head.sha,
+            status: 'completed',
+            started_at: timeStart,
+            conclusion: 'action_required',
+            completed_at: new Date(),
+            output: {
+              title: 'DCO',
+              summary
+            },
+            actions: [
+              {
+                label: 'Set DCO to pass',
+                description: 'would set status to passing',
+                identifier: 'override'
+              }
+            ]
+          })
+        )
+        .catch(function checkFails (error) {
+          if (error.status === 403) {
+            context.log.info('resource not accessible, creating status instead')
+            // create status
+            const description = dcoFailed[dcoFailed.length - 1].message.substring(0, 140)
+            const params = {
+              sha: pr.head.sha,
+              context: 'DCO',
+              state: 'failure',
+              description,
+              target_url: 'https://github.com/probot/dco#how-it-works'
+            }
+            return context.github.repos.createCommitStatus(context.repo(params))
+          }
+
+          throw error
+        })
     }
   }
 
   // This option is only presented to users with Write Access to the repo
   app.on('check_run.requested_action', setStatusPass)
   async function setStatusPass (context) {
-    try {
-      const timeStart = new Date()
+    const timeStart = new Date()
 
-      await context.github.checks.create(
-        context.repo({
-          name: 'DCO',
-          head_branch: context.payload.check_run.check_suite.head_branch,
-          head_sha: context.payload.check_run.head_sha,
-          status: 'completed',
-          started_at: timeStart,
-          conclusion: 'success',
-          completed_at: new Date(),
-          output: {
-            title: 'DCO',
-            summary: 'Commit sign-off was manually approved.'
-          }
-        })
-      )
-    } catch (error) {
-      console.error(error)
-    }
+    await context.github.checks.create(
+      context.repo({
+        name: 'DCO',
+        head_branch: context.payload.check_run.check_suite.head_branch,
+        head_sha: context.payload.check_run.head_sha,
+        status: 'completed',
+        started_at: timeStart,
+        conclusion: 'success',
+        completed_at: new Date(),
+        output: {
+          title: 'DCO',
+          summary: 'Commit sign-off was manually approved.'
+        }
+      })
+    )
   }
 }
 
